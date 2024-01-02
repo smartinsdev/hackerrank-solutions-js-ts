87070deecfba8e0003ed0e02516ba4d19021888a
smartinsdev <sinval.msjunior@gmail.com>
2024-01-03 00:56:17 +0100

    chore: remove unnecessary logs

--- src/week_1/divisible_sum_pairs/solution.ts
+++ src/week_1/divisible_sum_pairs/solution.ts
@@ -1,9 +1,7 @@
 export function divisibleSumPairs(_n: number, k: number, ar: number[]): number {
   let numberOfPairs = 0;
   while (ar.length >= 1) {
-    console.log(`tamanho da lista: ${ar.length}`);
     let popElement = ar.pop();
-    console.log(`elemento tirado: ${popElement}`);
     numberOfPairs += ar.reduce(
       (acc, value) => ((popElement! + value) % k === 0 ? ++acc : acc),
       0
