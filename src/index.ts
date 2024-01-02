94a05e630cf4f06d1050f6d8b88b0dc5c3b3d89f
smartinsdev <sinval.msjunior@gmail.com>
2024-01-03 00:48:11 +0100

    feat: implement main logic for handling multiple challenges

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// import { plusMinus } from './plus_minus/solution';
+// import { minMaxSum } from './mini_max_sum/solution';
+// import { timeConversion } from './time_conversion/solution';
+// import { breakingRecords } from './breaking_the_records/solution';
+// import { camelCase } from './camel_case_4/solution';
+// import { divisibleSumPairs } from './divisible_sum_pairs/solution';
+// import { matchingStrings } from './sparse_arrays/solution';
+
+// plusMinus()
+// minMaxSum()
+// timeConversion("07:05:45PM")
+// breakingRecords([10, 5, 20, 20, 4, 5, 2, 25, 1])
+// camelCase("C;M;white sheet of paper");
+// divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2]);
+// const strings = ['aba', 'baba', 'aba', 'xzxb'];
+// const queries = ['aba', 'xzxb', 'ab'];
+// matchingStrings(strings, queries);
